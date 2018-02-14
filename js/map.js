fb321b11993f9dc33b83a9d5f45ded271920b49e
akatov-an <alexeiakatov@gmail.com>
2018-02-14 17:00:11 +0500

    Исправлено по замечаниям, доделано по заданию module3-task1

--- js/map.js
+++ js/map.js
@@ -9,6 +9,7 @@ var TITLES = {
   bungalo: ['Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'],
   palace: ['Огромный прекрасный дворец', 'Маленький ужасный дворец']
 };
+var DESCRIPTION = '';
 var LOCATION_X_MIN = 300;
 var LOCATION_X_MAX = 900;
 var LOCATION_Y_MIN = 150;
@@ -38,97 +39,40 @@ var ANNOUNCEMENTS_COUNT = 8;
 var PIN_BUTTON_WIDTH = 40;
 var PIN_BUTTON_HEIGHT = 40;
 
-var ANNOUNCEMENT_CARD_TEMPLATE = document.querySelector('.announcementCardTemplate');
+var ANNOUNCEMENT_CARD_TEMPLATE = document.querySelector('template');
 
-
-// ФУНКЦИЯ: Устанавливает значение параметра 'announcement.author.avatar' в объекте announcement.
-var setAvatarInAnnouncement = function (announcement, avatarsLocation, currentAvatarCount) {
-  var avatarPath = avatarsLocation + 'user0' + currentAvatarCount + '.png';
-
-  announcement.author = {
-    "avatar": avatarPath
-  };
+// ФУНКЦИЯ: Возвращает значение для свойства 'announcement.offer.type'.
+var getHousingType = function (housingTypes) {
+  var randomIndex = window.utils.getRandomValue(0, housingTypes.length - 1, 0);
+  return housingTypes[randomIndex];
 };
 
-// ФУНКЦИЯ: Устанавливает значение параметра 'announcement.offer.type' в объекте announcement.
-var setHousingTypeInAnnouncement = function (announcement, housingTypes) {
-  var arrayLength = housingTypes.length;
-  var housingTypeIndex =  window.utils.getRandomValue(0, arrayLength - 1, 0);
-
-  var housingType = housingTypes[housingTypeIndex];
-  announcement.offer.type = housingTypes[housingTypeIndex];
+// ФУНКЦИЯ: Возвращает значение для свойства 'announcement.offer.title'.
+var getTitle = function (currentHousingType, titles) {
+  var randomIndex = window.utils.getRandomValue(0, titles[currentHousingType].length - 1, 0);
+  return titles[currentHousingType][randomIndex];
 };
 
-// ФУНКЦИЯ: Устанавливает значение параметра 'announcement.offer.title' в объекте announcement.
-var setTitleInAnnouncement = function (announcement, titles) {
-  var currentType = announcement.offer.type;
-  var specificHousingTitlesArrayLength = titles[currentType].length;
-  var housingTitleIndex = window.utils.getRandomValue(0, specificHousingTitlesArrayLength - 1, 0);
-
-  var currentHousingTitle = titles[currentType][housingTitleIndex];
-  announcement.offer.title = currentHousingTitle;
-};
-
-// ФУНКЦИЯ: Устанавливает значения параметров 'location.x' и 'location.y' в объекте announcement.
-var setLocationXandYinAnnouncement = function (announcement, xMin, xMax, yMin, yMax) {
-  announcement.location.x = window.utils.getRandomValue(xMin, xMax, 0);
-  announcement.location.y = window.utils.getRandomValue(yMin, yMax, 0);
-};
-
-// ФУНКЦИЯ: Устанавливает значение свойства 'announcement.offer.address'
-var setAddressInAnnouncement = function (announcement) {
-  var address = announcement.location.x + ' ' + announcement.location.y;
-
-  announcement.offer.address = address;
-};
-
-// ФУНКЦИЯ: Устанавливает значение свойства 'announcement.offer.price' в объекте announcement.
-var setPriceInAnnouncement = function (announcement, minPrice, maxPrice){
-  var currentPrice = window.utils.getRandomValue(minPrice, maxPrice, 0);
-  announcement.offer.price = currentPrice;
-};
-
-// ФУНКЦИЯ: Устанваливает значение свойства 'announcement.offer.rooms' в объекте announcement.
-var setRoomsCountInAnnouncement = function (announcement, minRooms, maxRooms){
-  var currentRoomsCount = window.utils.getRandomValue(minRooms, maxRooms, 0);
-  announcement.offer.rooms = currentRoomsCount;
-};
-
-// ФУНКЦИЯ: Устанавливает значение свойства 'announcement.offer.guests' в объекте announcement.
-var setAvailableGuestsCountInAnnouncememnt = function (announcement, minGuests, maxGuests){
-  var currentAvailableGuestsCount = window.utils.getRandomValue(minGuests, maxGuests, 0);
-  announcement.offer.guests = currentAvailableGuestsCount;
-};
-
-// ФУНКЦИЯ: Устанваливает значение свойств 'announcement.offer.checkin' и 'announcement.offer.checkout' в объекте announcement.
-var setCheckinAndCheckoutInAnnouncement = function (announcement, checkinVariants, checkoutVariants) {
-  var currentCheckinIndex = window.utils.getRandomValue(0, checkinVariants.length - 1, 0);
-  var currentCheckoutIndex = window.utils.getRandomValue(0, checkoutVariants.length - 1, 0);
-
-  announcement.offer.checkin = checkoutVariants[currentCheckinIndex];
-  announcement.offer.checkout = checkoutVariants[currentCheckoutIndex];
-};
-
-// ФУНКЦИЯ: Устанавливает значение свойства 'announcememnt.offer.photos'
-var setFeaturesInAnnouncememnt = function (announcememnt, featuresArray){
+// ФУНКЦИЯ: Возвращает значение свойства 'announcememnt.offer.features'
+var getFeaturesArray = function (basicFeaturesArray){
   var resultFeaturesArray = [];
 
-  for (var i = 0; i < featuresArray.length; i++){
+  for (var i = 0; i < basicFeaturesArray.length; i++){
     if(window.utils.getRandomValue(0, 1, 0)){
-      resultFeaturesArray.push(featuresArray[i]);
+      resultFeaturesArray.push(basicFeaturesArray[i]);
     }
   }
 
-  announcememnt.offer.features = resultFeaturesArray;
+  return resultFeaturesArray;
 };
 
-// ФУНКЦИЯ: Устанавливает значение свойства 'announcement.offer.photos' в объекте announcement.
-var setPhotosInAnnouncememnt = function (announcement, photosArray){
+// ФУНКЦИЯ: Возвращает значение свойства 'announcement.offer.photos'.
+var getPhotosArray = function (basicPhotosArray){
   var resultArray = [];
 
   // копирвоание переданного массива
-  for(var i = 0; i < photosArray.length; i++){
-    resultArray.push(photosArray[i]);
+  for(var i = 0; i < basicPhotosArray.length; i++){
+    resultArray.push(basicPhotosArray[i]);
   }
 
   // создание случайного порядка фотографий в массиве
@@ -142,58 +86,46 @@ var setPhotosInAnnouncememnt = function (announcement, photosArray){
     }
   }
 
-  announcement.offer.photos = resultArray;
+  return resultArray;
 };
 
 // ФУНКЦИЯ: Собирает объект announcement.
-var compileAnnouncement = function(){
+var createAnnouncement = function(usersAvatarsLocation, currentAvatarCount, housingTypes, titles, xMin, xMax, yMin, yMax,
+                                  minPrice, maxPrice, minRooms, maxRooms, minGuests, maxGuests, checkinVariants, checkoutVariants,
+                                  basicFeaturesArray, description, basicPhotosArray){
   var announcement = {
     'author': {
-      'avatar': null
-    },
-    'offer': {
-      'title': null,
-      'address': null,
-      'price': null,
-      'type': null,
-      'rooms': null,
-      'guests': null,
-      'checkin': null,
-      'checkout': null,
-      'features': null,
-      'description': null,
-      'photos': null
+      'avatar': usersAvatarsLocation + 'user0' + currentAvatarCount + '.png'
     },
     'location': {
-      'x': null,
-      'y': null
+      'x': window.utils.getRandomValue(xMin, xMax, 0),
+      'y': window.utils.getRandomValue(yMin, yMax, 0)
+    },
+    'offer': {
+      'type': getHousingType(housingTypes),
+      'title': null,
+      'address': null,
+      'price': window.utils.getRandomValue(minPrice, maxPrice, 0),
+      'rooms': window.utils.getRandomValue(minRooms, maxRooms, 0),
+      'guests': window.utils.getRandomValue(minGuests, maxGuests, 0),
+      'checkin': checkinVariants[window.utils.getRandomValue(0, checkinVariants.length - 1, 0)],
+      'checkout': checkoutVariants[window.utils.getRandomValue(0, checkoutVariants.length - 1, 0)],
+      'features': getFeaturesArray(basicFeaturesArray),
+      'description': description,
+      'photos': getPhotosArray(basicPhotosArray)
     }
   };
-
-  setAvatarInAnnouncement(announcement, USERS_AVATARS_LOCATION, ++CURRENT_AVATAR_COUNT);
-  setHousingTypeInAnnouncement(announcement, HOUSING_TYPES);
-  setTitleInAnnouncement(announcement,TITLES);
-  setLocationXandYinAnnouncement(announcement, LOCATION_X_MIN, LOCATION_X_MAX, LOCATION_Y_MIN, LOCATION_Y_MAX);
-  setAddressInAnnouncement(announcement);
-  setPriceInAnnouncement(announcement, MIN_PRICE, MAX_PRICE);
-  setRoomsCountInAnnouncement(announcement, MIN_ROOMS_COUNT, MAX_ROOMS_COUNT);
-  setAvailableGuestsCountInAnnouncememnt(announcement, MIN_GUESTS_COUNT, MAX_GUESTS_COUNT);
-  setCheckinAndCheckoutInAnnouncement(announcement, CHECKIN_VARIANTS, CHECKOUT_VARIANTS);
-  setFeaturesInAnnouncememnt(announcement, FEATURES);
-  setPhotosInAnnouncememnt(announcement, PHOTOS);
-
-  // Место для метода для установки значения announcement.offer.description
-
-  setPhotosInAnnouncememnt(announcement, PHOTOS);
+  announcement.offer.title = getTitle(announcement.offer.type, titles);
+  announcement.offer.address = announcement.location.x + ' ' + announcement.location.y;
 
   return announcement;
 };
 
 // ФУНКЦИЯ: Создает DOM-элемент метки.
 // возвращает настроенный и готовый для вставки DOM-элемент метки для карты
-var createDOMPinForAnnouncement = function (announcement, pinButtonWidth, pinButtoHeight) {
+var createDOMPinForAnnouncement = function (announcement, pinButtonWidth, pinButtonHeight) {
   var actualXPosition = announcement.location.x - pinButtonWidth/2;
-  var actualYPosition = announcement.location.y - pinButtoHeight;
+  var actualYPosition = announcement.location.y - pinButtonHeight;
 
   var button = document.createElement('button');
   var buttonStyleString = 'left:' + actualXPosition +'px; top:' + actualYPosition + 'px;';
@@ -203,7 +135,7 @@ var createDOMPinForAnnouncement = function (announcement, pinButtonWidth, pinBut
   var image = document.createElement('img');
   image.src = announcement.author.avatar;
   image.width = pinButtonWidth;
-  image.height = pinButtoHeight;
+  image.height = pinButtonHeight;
   image.setAttribute('draggable', 'false');
 
   button.appendChild(image);
@@ -211,20 +143,16 @@ var createDOMPinForAnnouncement = function (announcement, pinButtonWidth, pinBut
   return button;
 };
 
-// ФУНКЦИЯ: Возвращает DOM-элемент объявления, созданный на основании шаблона <template class='announcementCardTemplate' и
+// ФУНКЦИЯ: Возвращает DOM-элемент объявления, созданный на основании шаблона <template> (который в конце index.html) и
 // заполненный данными из объекта announcement.
 var createDOMElementForAnnouncement = function(announcement, domTemplate){
   var announcementCard = domTemplate.content.cloneNode(true);
-
   // title
-  announcementCard.querySelector('.cardTitle').textContent = announcement.offer.title;
-
+  announcementCard.querySelector('h3').textContent = announcement.offer.title;
   // address
-  //??????????????????????
-
+  announcementCard.querySelector('p > small').textContent = announcement.offer.address;
   // price
   announcementCard.querySelector('.popup__price').textContent = announcement.offer.price + '&#x20bd;/ночь';
-
   // housing type
   var housingTypeInRussian;
   switch (announcement.offer.type) {
@@ -237,39 +165,49 @@ var createDOMElementForAnnouncement = function(announcement, domTemplate){
     case 'palace':
       housingTypeInRussian = 'Дворец'; break;
   }
-  announcementCard.querySelector('.housingType').textContent = housingTypeInRussian;
-
+  announcementCard.querySelector('h4').textContent = housingTypeInRussian;
   // rooms and guests
-  announcementCard.querySelector('.roomsAndGuests').textContent = announcement.offer.rooms + ' комнаты для ' + announcement.offer.guests + ' гостей';
-
+  announcementCard.querySelector('p:nth-child(7)').textContent = announcement.offer.rooms + ' комнаты для ' + announcement.offer.guests + ' гостей';
   // checkIn, checkOut
-  announcementCard.querySelector('.checkinAndOut').textContent = 'Заезд после ' + announcement.offer.checkin + ', выезд до ' + announcement.offer.checkout;
-
+  announcementCard.querySelector('p:nth-child(8)').textContent = 'Заезд после ' + announcement.offer.checkin + ', выезд до ' + announcement.offer.checkout;
   // features
-  //?????????????????????????
-
+  var featurecContainer = announcementCard.querySelector('.popup__features');
+  var featuresInDOMArray = featurecContainer.children;
+  var isItemToDelete;
+  for(var i = featuresInDOMArray.length - 1; i > -1; i--){
+    var identifier = featuresInDOMArray[i].className.split('--')[1];
+     isItemToDelete = true;
+    for(var j = 0; j < announcement.offer.features.length; j++){
+      if(identifier === announcement.offer.features[j]){
+        isItemToDelete = false;
+        break;
+      }
+    }
+    if(isItemToDelete){
+      featuresInDOMArray[i].parentNode.removeChild(featuresInDOMArray[i]);
+    }
+  }
   // description
-  announcementCard.querySelector('.description').textContent = announcement.offer.description;
-
+  announcementCard.querySelector('p:nth-child(10)').textContent = announcement.offer.description;
   // pictures
   var picturesCount = announcement.offer.photos.length;
   var fragment = document.createDocumentFragment();
-  var listElementTemplateForPicture = announcementCard.querySelector('.imageListItem');
+  var listItemTemplate = announcementCard.querySelector('.popup__pictures > li');
   var newListItem;
-  var currentImage;
+  var nestedImage;
   for(var i = 0; i < picturesCount; i++){
-    newListItem = listElementTemplateForPicture.cloneNode(true);
-    currentImage = newListItem.querySelector('.announcmentImage');
-    currentImage.src = announcement.offer.photos[i];
-    currentImage.width = '50px';
-    currentImage.height = '50px';
+    newListItem = listItemTemplate.cloneNode(true);
+    nestedImage = newListItem.querySelector('img');
+    nestedImage.src = announcement.offer.photos[i];
+    nestedImage.width = '50px';
+    nestedImage.height = '50px';
     fragment.appendChild(newListItem);
   }
-  announcementCard.removeChild(listTemplateElementForPicture);
+  listItemTemplate.parentNode.removeChild(listItemTemplate);
   announcementCard.querySelector('.popup__pictures').appendChild(fragment);
-
   // avatar
   announcementCard.querySelector('.popup__avatar').src = announcement.author.avatar;
+  return announcementCard;
 
 };
 
@@ -277,28 +215,49 @@ var createDOMElementForAnnouncement = function(announcement, domTemplate){
 
 
 var main = function(){
+  // создание массива js-объектов объявлений
   var announcements = [];
   for(var i = 0; i < ANNOUNCEMENTS_COUNT ; i++){
-    announcements.push(compileAnnouncement());
+    announcements.push(createAnnouncement(USERS_AVATARS_LOCATION,
+      ++CURRENT_AVATAR_COUNT,
+      HOUSING_TYPES,
+      TITLES,
+      LOCATION_X_MIN, LOCATION_X_MAX,
+      LOCATION_Y_MIN, LOCATION_Y_MAX,
+      MIN_PRICE, MAX_PRICE,
+      MIN_ROOMS_COUNT, MAX_ROOMS_COUNT,
+      MIN_GUESTS_COUNT, MAX_GUESTS_COUNT,
+      CHECKIN_VARIANTS,CHECKOUT_VARIANTS,
+      FEATURES, DESCRIPTION , PHOTOS));
   }
 
+  // сделать карту доступной для работы с ней
   var map = document.querySelector('.map--faded');
   map.classList.remove('map--faded');
 
+  // создание documentFragment содержащий все метки-пины для карты
   var fragment = document.createDocumentFragment();
-
   var newPin;
   for (i = 0; i < announcements.length; i++) {
     newPin = createDOMPinForAnnouncement(announcements[i], PIN_BUTTON_WIDTH, PIN_BUTTON_HEIGHT);
     fragment.appendChild(newPin);
   }
 
+  // вставка меток-пинов на карту
   var pinsContainer = document.querySelector('.map__pins');
   pinsContainer.appendChild(fragment);
 
-  // создать и заполнить DOM-элемент объявления
+  // создание и заполнение DOM-элементов объявлений
+  var fragment = document.createDocumentFragment();
+  var domElementForAnnouncement;
 
+  for (i = 0; i < announcements.length; i++){
+    domElementForAnnouncement = createDOMElementForAnnouncement(announcements[i], ANNOUNCEMENT_CARD_TEMPLATE);
+    fragment.appendChild(domElementForAnnouncement);
+    console.log('created dom elements: ' + i);
+  }
 
-
+  var container = document.querySelector('.map');
+  container.insertBefore(fragment, container.querySelector('.map__filters-container'));
 
 };
